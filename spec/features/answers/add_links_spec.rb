00225d4f77be2a01496f1a274c5ddae8e72ac267
Dmitrii <veter.666@list.ru>
2019-01-15 20:03:43 +1000

    Nested forms, polymoprphic: User can add links to questions and answers 2

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -9,6 +9,7 @@ feature 'User can add links to answer', %q{
   given(:user) {create(:user)}
   given!(:question) {create(:question, author: user)}
   given(:gist_url) {'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c'}
+  given(:gist_url_2) { 'https://gist.github.com/neverovda/c98d3a71503d2a08b8576a9e7483dcbb' }
 
   scenario 'User adds link when give an answer', js: true do
     sign_in(user)
@@ -17,13 +18,23 @@ feature 'User can add links to answer', %q{
 
     fill_in 'Your answer', with: 'My answer'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    click_on 'Add link'
+    within('#links nested-fields:nth-child(1)') do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+    end  
+    
+    click_on 'Add link'
+    within('#links nested-fields:nth-child(2)') do
+      fill_in 'Link name', with: 'My gist 2'
+      fill_in 'Url', with: gist_url_2
+    end
 
     click_on 'Post your answer'
 
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'My gist 2', href: gist_url_2
     end
   end
 
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -8,20 +8,32 @@ feature 'User can add links to question', %q{
 
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c' }
+  given(:gist_url_2) { 'https://gist.github.com/neverovda/c98d3a71503d2a08b8576a9e7483dcbb' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds link when asks question', js: true do
     sign_in(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    
+    click_on 'Add link'
+    within('#links nested-fields:nth-child(1)') do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+    end  
+    
+    click_on 'Add link'
+    within('#links nested-fields:nth-child(2)') do
+      fill_in 'Link name', with: 'My gist 2'
+      fill_in 'Url', with: gist_url_2
+    end
 
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My gist 2', href: gist_url_2
   end
 
-end 
+end
